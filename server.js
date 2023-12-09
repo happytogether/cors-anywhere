06ffa8e62c1d040569ad9db86e5076d338a09892
happytogether <happytogether.annie@gmail.com>
2023-12-09 21:20:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var fs = require("fs");
+
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
@@ -64,7 +66,11 @@ cors_proxy
       // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
       xfwd: false,
     },
-    httpsOptions: {},
+    httpsOptions: {
+      ///etc/secrets/<filename>
+      key: fs.readFileSync("/etc/secrets/key.pem"),
+      cert: fs.readFileSync("/etc/secrets/cert.pem"),
+    },
   })
   .listen(port, host, function () {
     console.log("Running CORS Anywhere on " + host + ":" + port);
